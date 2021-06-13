a1279183fa30cd94ffcff4c033d942c007934c59
swagere <1250539180@qq.com>
2021-06-13 18:33:28 +0800

    add 279_2,279_3

--- Leetcode/src/leetcode/p_279_2.java
+++ Leetcode/src/leetcode/p_279_2.java
@@ -7,28 +7,31 @@ import java.util.Scanner;
 public class p_279_2 {
 	static int[] array;
 	
-	//普通递归 + 记忆化搜索：存储已计算过的值
-	 public static int numSquares(int n) {
-		 if (n == 0) {
+	//普通递归 + 记忆化搜索：存储已计算过的值  + min的使用
+	public static int numSquares(int n) {
+		array = new int[n + 1]; //全部初始化为0
+		return f(n);
+	}
+	
+	public static int f(int n) {
+		if (n == 0) {
 			 return n;
 		 }else if (array[n] != 0) {
 			 return array[n];
 		 }
 		 
 		 //如果n为平方数
-		 for (int i = 1; i <= Math.sqrt(n); i++) {
-			 if (i*i == n) {
-				 return 1;
-			 }
+		 int temp = (int)Math.sqrt(n);
+		 if (temp * temp == n) {
+			 return 1;
 		 }
 		 
-		 
-		 List<Integer> res = new ArrayList<Integer>();
-		 for (int i = 1; i <= Math.sqrt(n); i++) {
-			 res.add(numSquares(i * i) + numSquares(n - i * i));
+		 List<Integer> res  = new ArrayList<>();
+		 for (int i = 1; i < Math.sqrt(n); i++) {
+			 res.add(f(i * i) + f(n - i * i));
 		 }
-
-		 //找到res中最小的
+		 
+		//找到res中最小的
 		 int min = res.get(0);
 		 for (int i : res) {
 			 if (i < min) {
@@ -36,15 +39,12 @@ public class p_279_2 {
 			 }
 		 }
 		 
-		 array[n] = min;
-		 
-		 return min;
+		 return array[n];
 	}
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		array = new int[n + 1];
 		System.out.println(numSquares(n));
 	}
 	
