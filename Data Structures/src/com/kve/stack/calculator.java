25ae03b0d5c5784418d8c4d9bf67249fe7fd754a
swagere <1250539180@qq.com>
2021-07-10 12:01:25 +0800

    add ( and ) to calculator

--- Data Structures/src/com/kve/stack/calculator.java
+++ Data Structures/src/com/kve/stack/calculator.java
@@ -3,6 +3,10 @@ package com.kve.stack;
 //int char可以相互转化
 //用char直接存成int时，若是数字则要减去ASCII码（-48），如要存'8'，则需'8'-48
 
+//+-*/
+//多位运算
+//()
+
 public class calculator {
 	//单次计算 
 	//输入:两个数和一个操作符
@@ -22,7 +26,7 @@ public class calculator {
 	
 	//优先级是否大于
 	public static boolean biggerthan(char a, String b) {
-		if ((a == '*' || a == '/') && (b.equals("+") || b.equals("-"))) {
+		if (((a == '*' || a == '/') && (b.equals("+") || b.equals("-"))) || a == '(' || b.equals("(")) {
 			return true;
 		}
 		return false;
@@ -39,6 +43,22 @@ public class calculator {
 			char cur = str.charAt(i);
 			if (isOper(cur)) {
 				//如果是运算符
+				
+				//如果是)符号
+				if (cur == ')') {
+					//则计算括号中的
+					while (!operStack.getPop().equals("(")) {
+						String oper = operStack.pop();
+						String num1 = numStack.pop();
+						String num2 = numStack.pop();
+						
+						double res = calculatorSingle(num1, num2, oper);
+						numStack.push(String.valueOf(res));
+					}
+					operStack.pop(); //将(出栈
+					continue;
+				}
+				
 				//如果栈为空 或 运算符优先级大于栈顶元素优先级 则直接进栈
 				if (operStack.isEmpty() || biggerthan(cur, operStack.getPop())) {
 					operStack.push(String.valueOf(cur));
@@ -83,14 +103,14 @@ public class calculator {
 	}
 
 	private static boolean isOper(char cur) {
-		if (cur == '+' || cur == '-' || cur == '*' || cur == '/') {
+		if (cur == '+' || cur == '-' || cur == '*' || cur == '/' || cur == '(' || cur == ')') {
 			return true;
 		}
 		return false;
 	}
 
 	public static void main(String[] args) {
-		String str = "6+3*3-100";
+		String str = "(6+3*3)*3-10";
 		System.out.println(calculator(str));
 		
 
