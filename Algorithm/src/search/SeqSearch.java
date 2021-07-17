bb1f94a88408b585a7b340e81e09015751343b4f
swagere <1250539180@qq.com>
2021-07-17 10:40:02 +0800

    add l_1818 add SeqSearch

---
+++ Algorithm/src/search/SeqSearch.java
@@ -0,0 +1,22 @@
+package search;
+
+public class SeqSearch {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5,6,2};
+
+		System.out.println(seqSearch(nums, 2));
+	}
+
+	//线性查找
+	//找到一个就返回 返回值为下标
+	private static int seqSearch(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			if (i == target) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
